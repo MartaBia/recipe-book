8b58851c0f309a1c15d67d214299ede535250f2c
Marta Bianchini <marta.bianchini24@gmail.com>
2023-05-01 17:08:07 +0100

    amended method get by id with get by name

--- backend/recipe_repository.py
+++ backend/recipe_repository.py
@@ -1,6 +1,7 @@
 import psycopg2
 from recipe import Recipe
 
+
 class RecipeRepository:
     def __init__(self, connection_string):
         self.connection_string = connection_string
@@ -12,23 +13,25 @@ class RecipeRepository:
                 rows = cur.fetchall()
                 recipes = [Recipe(*row) for row in rows]
                 return recipes
-    
-    def get_from_id(self, recipe_id):
+                # Returns a Recipe objects list
+
+    def get_by_name(self, recipe_name):
         with psycopg2.connect(self.connection_string) as conn:
             with conn.cursor() as cur:
-                cur.execute("SELECT * FROM recipes WHERE id=%s", (recipe_id,))
+                cur.execute("SELECT * FROM recipes WHERE name=%s",
+                            (recipe_name,))
                 row = cur.fetchone()
                 if row:
                     return Recipe(*row)
                 else:
                     return None
-                
+                # Returns a Recipe object
+
     def add(self, name, ingredients, process):
         with psycopg2.connect(self.connection_string) as conn:
             with conn.cursor() as cur:
-                cur.execute("INSERT INTO recipes (name, ingredients, process) VALUES (%s, %s, %s) RETURNING id", (name, ingredients, process))
+                cur.execute(
+                    "INSERT INTO recipes (name, ingredients, process) VALUES (%s, %s, %s) RETURNING id", (name, ingredients, process))
                 recipe_id = cur.fetchone()[0]
                 return Recipe(recipe_id, name, ingredients, process)
-    
-
-      
+                # Returns the newly created Recipe object with its id attribute set
